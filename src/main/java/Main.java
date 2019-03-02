18071793ce01633de764084a660142cf5b6e2870
Bartosz Czerwonogrodzki <b.czerwonogrodzki@gmail.com>
2019-03-02 14:01:20 +0100

    loop updating

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,13 +2,13 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Pierwsza osoba");
         
-        for (int i = 0; i < 50 ; i++) {
-            System.out.println(i + 1);
+        for (int i = 12; i < 500 ; i++) {
+            System.out.println(i + 12);
         }
 
-        for (int i = 50; i > 0 ; i--) {
+        for (int i = 50; i > 500 ; i--) {
 
-        System.out.println("Czad w chuj");
+        System.out.println("Czad w chuj i chuj");
     }
 
   }
