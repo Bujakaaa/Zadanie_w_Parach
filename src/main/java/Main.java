a5e1b4cf4fd45ffa2f8e35584b62de371164bb3d
Paulina Bujała <bujala.paulina@gmail.com>
2019-03-02 13:33:57 +0100

    Dodanie drugij petli

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,5 +3,9 @@ public class Main {
         for (int i = 0; i <= 100 ; i++) {
             System.out.println(i);
         }
+
+        for (int i = 100; i >0 ; i--) {
+            System.out.println(i);
+        }
     }
 }
