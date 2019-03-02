41781d52e66ed8712e835d2752bb71cc29308a58
Bartosz Czerwonogrodzki <b.czerwonogrodzki@gmail.com>
2019-03-02 13:36:55 +0100

    comment added

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,11 +1,13 @@
 public class Main {
     public static void main(String[] args) {
-        for (int i = 0; i <= 100 ; i++) {
+        for (int i = 0; i < 100 ; i++) {
+            System.out.println(i + 1);
+        }
+
+        for (int i = 100; i > 0 ; i--) {
             System.out.println(i);
         }
 
-        for (int i = 100; i >0 ; i--) {
-            System.out.println(i);
-        }
+        System.out.println("Czad");
     }
 }
